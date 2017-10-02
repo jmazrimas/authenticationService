2d953d570c9c1c057287561c3a4262c4b901d03c
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-10-02 12:00:54 -0500

    remove commented out code

--- app.py
+++ app.py
@@ -1,54 +1,9 @@
 from flask import Flask, request, jsonify, make_response, render_template
-# import urlparse
-# from urllib import urlencode
-# import os
 import socket
 import requests
-
 import google_auth
 
-# Connect to Redis
-# redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
 app = Flask(__name__)
-
-# def getGoogleKeys():
-#     google_keys = {}
-#     google_keys['client']=os.getenv('GOOGLE_OAUTH_CLIENT_ID')
-#     google_keys['secret']=os.getenv('GOOGLE_OAUTH_SECRET')
-#     return google_keys
-#
-# google_keys = getGoogleKeys()
-#
-# def generate_login_url():
-#     base_url = "https://accounts.google.com/o/oauth2/v2/auth"
-#     parsed_url = list(urlparse.urlparse(base_url))
-#     params = {
-#         'redirect_uri': 'http://localhost:8090/login-callback',
-#         'scope': 'profile',
-#         'access_type': 'offline',
-#         'response_type': 'code',
-#         'client_id': google_keys['client'],
-#         'prompt': 'consent'
-#     }
-#     parsed_url[4] = urlencode(params)
-#     return urlparse.urlunparse(parsed_url)
-#
-# login_url = generate_login_url()
-# print '\n\n\n\nLOGIN URL', login_url
-#
-# def generate_token_url(code):
-#     base_url = "https://www.googleapis.com/oauth2/v4/token"
-#     parsed_url = list(urlparse.urlparse(base_url))
-#     params = {
-#         'code': code,
-#         'client_id': google_keys['client'],
-#         'client_secret': google_keys['secret'],
-#         'redirect_uri': 'http://localhost:8090/login-callback',
-#         'grant_type': 'authorization_code'
-#     }
-#     parsed_url[4] = urlencode(params)
-#     return urlparse.urlunparse(parsed_url)
 
 @app.route("/")
 def main():
