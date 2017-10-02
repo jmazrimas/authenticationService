ba2f476d6e89d1f934939b1cf5a270378e34a313
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-10-02 11:22:39 -0500

    able to exchange for token -- still need to test token

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import urlparse
 from urllib import urlencode
 import os
 import socket
+import requests
 
 # Connect to Redis
 # redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
@@ -17,19 +18,35 @@ def getGoogleKeys():
 
 google_keys = getGoogleKeys()
 
-def generateLoginURL():
+def generate_login_url():
     base_url = "https://accounts.google.com/o/oauth2/v2/auth"
     parsed_url = list(urlparse.urlparse(base_url))
     params = {
         'redirect_uri': 'http://localhost:8090/login-callback',
+        'scope': 'profile',
+        'access_type': 'offline',
         'response_type': 'code',
         'client_id': google_keys['client'],
-        'scope': 'profile'
+        'prompt': 'consent'
     }
     parsed_url[4] = urlencode(params)
     return urlparse.urlunparse(parsed_url)
 
-loginURL = generateLoginURL()
+login_url = generate_login_url()
+print '\n\n\n\nLOGIN URL', login_url
+
+def generate_token_url(code):
+    base_url = "https://www.googleapis.com/oauth2/v4/token"
+    parsed_url = list(urlparse.urlparse(base_url))
+    params = {
+        'code': code,
+        'client_id': google_keys['client'],
+        'client_secret': google_keys['secret'],
+        'redirect_uri': 'http://localhost:8090/login-callback',
+        'grant_type': 'authorization_code'
+    }
+    parsed_url[4] = urlencode(params)
+    return urlparse.urlunparse(parsed_url)
 
 @app.route("/")
 def main():
@@ -44,11 +61,22 @@ def main():
 
 @app.route("/login")
 def login():
-    return render_template('login.html', loginURL=loginURL)
+    return render_template('login.html', login_url=login_url)
 
 @app.route("/login-callback")
 def login_callback():
-    return render_template('login-callback.html', loginURL=loginURL)
+    code = request.args.get('code')
+    token_url = generate_token_url(code)
+    res = requests.post(token_url)
+    access_token = res.json()['access_token']
+    expires_in = res.json()['expires_in']
+    refresh_token = res.json()['refresh_token']
+
+    print 'access_token:', access_token
+    print 'expires_in:', expires_in
+    print 'refresh_token:', refresh_token
+
+    return render_template('login-callback.html', login_url=login_url)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8090)
