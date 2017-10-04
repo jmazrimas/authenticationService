57435f3142d3e1e695c895b06c87d5f4f0696609
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-10-04 10:11:25 -0500

    update to more accurate schema

--- flask/authentication_data_model.py
+++ flask/authentication_data_model.py
@@ -10,7 +10,13 @@ class AuthenticationDataModel:
 
         class User(db.Model):
             id = db.Column(db.Integer, primary_key=True)
-            name = db.Column(db.String(80), unique=True, nullable=False)
+            name = db.Column(db.String(256), nullable=False)
+            third_party = db.Column(db.String(256), nullable=False)
+            third_party_id = db.Column(db.String(256), nullable=False)
+            access_key = db.Column(db.String(256), nullable=False)
+            renew_key = db.Column(db.String(256), nullable=False)
+            session = db.Column(db.String(256), nullable=False)
+            expire_time = db.Column(db.DateTime, nullable=False)
 
             def __repr__(self):
                 return '<User %r>' % self.name
