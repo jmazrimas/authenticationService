026925955d939b9e6c57bfbe6365dd3e59c50eba
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-10-04 10:54:54 -0500

    refactor to new pattern with routes still working

--- flask/app.py
+++ flask/app.py
@@ -1,39 +1,64 @@
-from flask import Flask, request, jsonify, make_response, render_template
-import socket
-import requests
-import google_auth
-from authentication_data_model import AuthenticationDataModel
+# from flask import Flask, request, jsonify, make_response, render_template
+# import socket
+# import requests
+# import google_auth
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://authentication:authenticationpass@localhost/authentication'
+# db = SQLAlchemy(app)
+#
+# import models
+#
+# db.create_all()
+#
+# @app.route("/")
+# def main():
+#     data = {'testJsonKey': 'testJsonValue NEW'}
+#     response = make_response(jsonify(data))
+#     response.set_cookie('username', 'the username')
+#     return response
+#
+# @app.route("/login")
+# def login():
+#     return render_template('login.html', login_url=google_auth.login_url)
+#
+# @app.route("/login-callback")
+# def login_callback():
+#     code = request.args.get('code')
+#     token_url = google_auth.generate_token_url(code)
+#     res = requests.post(token_url)
+#     access_token = res.json()['access_token']
+#     expires_in = res.json()['expires_in']
+#     refresh_token = res.json()['refresh_token']
+#
+#     user_info = google_auth.get_user_info(access_token)
+#
+#     return render_template(
+#         'login-callback.html',
+#         user_name=user_info['name'],
+#         user_id=user_info['user_id'])
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8090)
+#
 
-app = Flask(__name__)
-adm = AuthenticationDataModel(app)
 
-@app.route("/")
-def main():
-    data = {'testJsonKey': 'testJsonValue NEW'}
-    response = make_response(jsonify(data))
-    response.set_cookie('username', 'the username')
-    return response
+from flask import Flask
+# from database import init_engine, init_db
 
-@app.route("/login")
-def login():
-    return render_template('login.html', login_url=google_auth.login_url)
 
-@app.route("/login-callback")
-def login_callback():
-    code = request.args.get('code')
-    token_url = google_auth.generate_token_url(code)
-    res = requests.post(token_url)
-    access_token = res.json()['access_token']
-    expires_in = res.json()['expires_in']
-    refresh_token = res.json()['refresh_token']
+def init_app():
+    print 'init app called'
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://authentication:authenticationpass@localhost/authentication'
+    # init_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    # init_db()
+    from authentication import authentication
+    app.register_blueprint(authentication)
 
-    user_info = google_auth.get_user_info(access_token)
+    return app
 
-    return render_template(
-        'login-callback.html',
-        user_name=user_info['name'],
-        user_id=user_info['user_id'])
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8090)
-
+if __name__ == '__main__':
+    app = init_app()
+    app.run(host='0.0.0.0', port=8090, debug=True)
---
+++ flask/authentication.py
@@ -0,0 +1,34 @@
+from flask import request, current_app, Blueprint, render_template
+# from database import db_session
+# from model import Product
+import requests
+import google_auth
+
+authentication = Blueprint('authentication', __name__)
+
+@authentication.route("/")
+def main():
+    data = {'testJsonKey': 'testJsonValue NEW'}
+    response = make_response(jsonify(data))
+    response.set_cookie('username', 'the username')
+    return response
+
+@authentication.route("/login")
+def login():
+    return render_template('login.html', login_url=google_auth.login_url)
+
+@authentication.route("/login-callback")
+def login_callback():
+    code = request.args.get('code')
+    token_url = google_auth.generate_token_url(code)
+    res = requests.post(token_url)
+    access_token = res.json()['access_token']
+    expires_in = res.json()['expires_in']
+    refresh_token = res.json()['refresh_token']
+
+    user_info = google_auth.get_user_info(access_token)
+
+    return render_template(
+        'login-callback.html',
+        user_name=user_info['name'],
+        user_id=user_info['user_id'])
